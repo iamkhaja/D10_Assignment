d1f839090785045bdc3a0af738e51130cf079c13
Khaja Nazmuddin Shaik <iamkhaja0786@gmail.com>
2022-03-29 18:04:07 +0530

    commit

---
+++ 21st_Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _21st_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int i, j;
+            for (i = 65; i <= 69; i++)
+            {
+                j = i + 32;
+                Console.Write("" +Convert.ToChar(i) + Convert.ToChar(j) +  " ");
+            }
+
+        }
+    }
+}
---
+++ 28Th_Assignments/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _28Th_Assignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int temp = 1;
+            int  rownum= 5;
+            while (rownum >=1)
+            {
+                int colnum = 1;
+                while (colnum < temp)
+                {
+                    Console.Write(" ");
+                        colnum++;
+                }
+                    {
+                    int starcount = 1;
+                    while (starcount <= 2 * rownum - 1)
+                    {
+                        starcount++;
+                        Console.Write("*");
+
+                    }
+                    temp++;
+                    rownum--;
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
